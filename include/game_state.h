45014e28be025a2d266aee4e60838875244e80c6
marr64 <127711549+MarrMalleville@users.noreply.github.com>
2026-03-24 17:13:11 -0300

    struct jugador agregada al game state

--- include/game_state.h
+++ include/game_state.h
@@ -1,1 +1,15 @@
 // structs compartidos (tablero, jugadores)
+#include <sys/types.h> // para pid_t
+#include <stdbool.h>    // para bool
+
+//struct dada por la cátedra
+typedef struct {
+    char players_name[16];        // Nombre del jugador
+    unsigned int score;    // Puntaje
+    unsigned int invalid_moves;    // Movimientos inválidos
+    unsigned int valid_moves;    // Movimientos válidos
+    unsigned short x, y; // Coordenadas x e y
+    pid_t pid;           // PID
+    bool blocked;            // Si está bloqueado
+} player_t;
+
